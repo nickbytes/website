2f7d523c5ead3a9eee52fbaa64c084016ef2568d
Nick Beattie <nick@nickbytes.com>
2018-10-16 09:22:41 -0400

    github

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Link from "gatsby-link";
-import { Headline, Wrapper, Sub, Container } from "./styles";
+import React from 'react'
+import Link from 'gatsby-link'
+import { Headline, Wrapper, Sub, Container } from './styles'
 
 const Header = () => (
   <Wrapper>
@@ -16,12 +16,17 @@ const Header = () => (
       <Sub>
         Here's a couple of the projects I've been involved with over the last
         few years. I've written some small summaries of them, but feel free to
-        reach out over <a href="mailto:nick@nickbytes.com">email</a> or{" "}
+        reach out over <a href="mailto:nick@nickbytes.com">email</a> or{' '}
         <a href="http://twitter.com/nicktweattie">twitter</a> if you'd like to
         chat more.
       </Sub>
+
+      <Sub>
+        More code can be viewed on{' '}
+        <a href="https://github.com/nickbytes">github</a>.
+      </Sub>
     </Container>
   </Wrapper>
-);
+)
 
-export default Header;
+export default Header
